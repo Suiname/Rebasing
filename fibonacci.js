21b687cfbe25c7234ec2cb4dcb71b81c9f84380f
Jason Tham <jason@moonrisers.com>
2018-09-17 10:01:06 -0500

    less than zero

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,10 @@
 function fibonacci(num){
 	if (typeof num !== 'number') {
 		return 'Nope';
-	}
+	} else if (num < 0 ) {
+		return 'Less than zero';
+	} 
+
 	var a = 1, b = 0, temp;
   
 	while (num >= 0){
